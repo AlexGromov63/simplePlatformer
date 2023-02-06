d76ba2df8b6167ae98a0133fe76b1322da2f1a4a
Your Name <you@example.com>
2023-02-06 20:24:33 +0300

    init

---
+++ Assets/Course Library/scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody playerRB;
+    public float jumpForce = 10;
+    public float gravityModifier = 10;
+    private bool isStay = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody>();
+        //playerRB.AddForce(Vector3.up * 500);
+        Physics.gravity *= gravityModifier;
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && isStay == true )
+        {
+            playerRB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isStay = false;
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        isStay = true;
+    }
+}
---
+++ Assets/Course Library/scripts/spawnManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnManager : MonoBehaviour
+{
+    public List<GameObject> prefabs;
+    public Vector3 spawnPos = new Vector3(25, 0, 0);
+
+    public float startDelay = 2;
+    public float repeatRate = 2;
+    public float liveTime = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Spawn", startDelay, repeatRate);
+        Invoke("Destroy", liveTime);
+    }
+    void Spawn()
+    {
+        var go  = Instantiate(prefabs[0], spawnPos, prefabs[0].transform.rotation);
+        Destroy(go, liveTime);
+
+        var prfb = prefabs[0];
+        prefabs.RemoveAt(0);
+        prefabs.Add(prfb);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
